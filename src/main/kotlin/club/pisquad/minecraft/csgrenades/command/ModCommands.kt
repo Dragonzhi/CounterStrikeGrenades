50b7ac8e5ed2a882c0e2b0b0e8739e62ab608a98
hukening <kening.hu@outlook.com>
2025-02-03 14:02:35 +0800

    add a command for ignoring barrier block

---
+++ src/main/kotlin/club/pisquad/minecraft/csgrenades/command/ModCommands.kt
@@ -0,0 +1,11 @@
+package club.pisquad.minecraft.csgrenades.command
+
+import net.minecraftforge.event.RegisterCommandsEvent
+import net.minecraftforge.eventbus.api.SubscribeEvent
+
+object ModCommands {
+    @SubscribeEvent
+    fun onRegisterCommands(event: RegisterCommandsEvent) {
+        ChangeSettingCommand.register(event.dispatcher)
+    }
+}
