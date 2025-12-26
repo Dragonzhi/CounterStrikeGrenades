13c6c6d52892e7f414c96b00a457d6a90f314cf3
Dragonzhi <3248569738@qq.com>
2025-12-26 11:56:45 +0800

    feat(client): 添加投掷物投出的模型（无拉环）并注册和渲染功能
    - 创建 ModClientEvents 类用于注册所有投掷物的附加模型
    - 修改 GrenadeRenderer 以使用自定义的投掷状态模型
    - 添加了 decoy_t.json 投掷诱饵弹的3D模型文件
    - 添加了 flashbang_t.json 投掷闪光弹的3D模型文件
    - 添加了 hegrenade_t.json 投掷手雷的3D模型文件
    - 添加了 incendiary_t.json 投掷燃烧弹的3D模型文件
    - 添加了 molotov_t.json 投掷莫洛托夫的3D模型文件
    - 添加了 smokegrenade_t.json 投掷烟雾弹的3D模型文件
    - 实现了模型加载和渲染的回退机制以确保兼容性

---
+++ src/main/kotlin/club/pisquad/minecraft/csgrenades/client/ModClientEvents.kt
@@ -0,0 +1,31 @@
+package club.pisquad.minecraft.csgrenades.client
+
+import club.pisquad.minecraft.csgrenades.CounterStrikeGrenades
+import club.pisquad.minecraft.csgrenades.registery.ModItems
+import net.minecraft.resources.ResourceLocation
+import net.minecraftforge.api.distmarker.Dist
+import net.minecraftforge.client.event.ModelEvent
+import net.minecraftforge.eventbus.api.SubscribeEvent
+import net.minecraftforge.fml.common.Mod
+
+@Mod.EventBusSubscriber(modid = CounterStrikeGrenades.ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = [Dist.CLIENT])
+object ModClientEvents {
+
+    @SubscribeEvent
+    fun onModelsRegistered(event: ModelEvent.RegisterAdditional) {
+        val grenadeItems = listOf(
+            ModItems.HEGRENADE_ITEM,
+            ModItems.FLASH_BANG_ITEM,
+            ModItems.SMOKE_GRENADE_ITEM,
+            ModItems.INCENDIARY_ITEM,
+            ModItems.MOLOTOV_ITEM,
+            ModItems.DECOY_GRENADE_ITEM
+        )
+
+        grenadeItems.forEach { item ->
+            val itemName = item.id.path
+            val thrownModelLocation = ResourceLocation(CounterStrikeGrenades.ID, "item/${itemName}_t")
+            event.register(thrownModelLocation)
+        }
+    }
+}
